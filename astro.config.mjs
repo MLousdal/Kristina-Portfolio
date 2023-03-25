25cd31b99e1d29380b506daa3be30bd11538ed6e
Mads Lousdal <74186479+MLousdal@users.noreply.github.com>
2023-03-25 17:17:28 +0100

    Image component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({ config: { applyBaseStyles: false }, }), sitemap()],
-  experimental: {
-    assets: true
-  }
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), sitemap(), image()]
 });
